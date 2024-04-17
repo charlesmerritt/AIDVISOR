7363c2feef2a3d0c8ba7b2fec4567b9b193c2e97
Charles Merritt <cem96047@uga.edu>
2024-04-16 22:59:43 -0400

    working

--- AIDVISOR/AidSearch/urls.py
+++ AIDVISOR/AidSearch/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('history/', views.history, name='history'),
     path('saved/', views.saved, name='saved'),
     path('myaccount/', views.myaccount, name='myaccount'),
-    path("create/", views.create, name="create"),
     path('resources/', views.resources, name='resources'),
     path('api/assistant-query/', views.assistant_query, name='assistant_query'),
+    path('api/save-scholarship/', views.save_scholarship, name='save_scholarship'),
+    path('api/edit-user-details', views.edit_user_details, name='edit_user_details'),
 ]
